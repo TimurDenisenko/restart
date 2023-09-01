e443e21915dd51462844ac6ae975f161a1df61d2
opilane <opilane@HP-CZC2349HTY>
2023-09-01 10:54:37 +0300

    v2

---
+++ Message.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restart
+{
+    public class Message
+    {
+        private readonly string _content;
+        private readonly string _author;
+        private readonly DateTime _time;
+        private int _likes;
+
+        public Message(string content, string author, DateTime time)
+        {
+            this._content = content;
+            this._author = author;
+            this._time = time;
+        }
+
+        public int Likes { get => _likes; }
+        public DateTime Time { get => _time; }
+        public string Author { get => _author; }
+        public string Content { get => _content; }
+
+        public void AddLike()
+        {
+            _likes++;
+        }
+
+        public double GetPopularity()
+        {
+            double elapsed = DateTime.Now.Subtract(this._time).TotalSeconds;
+            if (elapsed == 0)
+            {
+                return _likes;
+            }
+            return _likes / elapsed;
+
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("\n("+Author+") - "+Content+"\nTime: "+Time+"\nLikes: "+Likes+"\nPopularity: "+GetPopularity()+"\n");
+        }
+
+        public void WhoIsPopularity(Message m2)
+        {
+            string p = "";
+            if (Math.Round(GetPopularity(), 6)==Math.Round(m2.GetPopularity(), 6))
+            {
+                p="Mitte keegi";
+            }
+            else if (GetPopularity()>m2.GetPopularity())
+            {
+                p=Author;
+            }
+            else
+            {
+                p=m2.Author;
+            }
+            Console.WriteLine("{0} sõnum on populaarsem", p);
+        }
+
+        public void AddMessage()
+        {
+            
+        }
+
+        public void WhoIsPopularityN(List<Message> messages)
+        {
+            double p = 0;
+            List<Message> messages2 = new List<Message>();
+            for (int i = 0; i < messages.Count(); i++)
+            {
+                if (Math.Round(messages[i].GetPopularity(),6)==Math.Round(p, 6))
+                {
+                    messages2.Add(messages[i]);
+                }
+                else if (messages[i].GetPopularity()>p)
+                {
+                    messages2.Clear();
+                    p=messages[i].GetPopularity();
+                }
+            }
+            if (messages2.Count==1)
+            {
+                Console.WriteLine("{0} sõnum on populaarsem", messages2[0].Content);
+            }
+            else if (messages2.Count==2)
+            {
+                Console.WriteLine("{0} ja {1} sõnumeid on populaarsem", messages2[0].Content, messages2[1].Content);
+            }
+            else if (messages2.Count==3)
+            {
+                Console.WriteLine("{0}, {1} ja {2} sõnumeid on populaarsem", messages2[0].Content, messages2[1].Content, messages2[2].Content);
+            }
+            else
+            {
+                foreach (Message item in messages2)
+                {
+                    Console.Write("{0}, ",item.Content);
+                }
+                Console.WriteLine("sõnumeid on populaarsem");
+            }
+        }
+    }
+}
---
+++ Naidisid.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restart
+{
+    public class Naidisid
+    {
+        public static void Main(string[] args)
+        {
+            /*
+            Student studentJohn = new Student("John", 18, "London");
+
+            Console.WriteLine(studentJohn.Name);
+            Console.WriteLine(studentJohn.Age);
+            Console.WriteLine(studentJohn.GetCity());
+            Console.WriteLine(studentJohn.Status);
+            Console.WriteLine(studentJohn.BornYear());
+
+            Console.WriteLine();
+
+            studentJohn.ShowInfo();
+            */
+
+
+
+            Message m1 = new Message("Hello", "John", DateTime.Now.AddDays(-10));
+            m1.ShowInfo();
+            m1.AddLike();
+
+            Message m2 = new Message("Hi", "Mary", DateTime.Now.AddMinutes(-13));
+            Message m3 = new Message("H1i", "Mary", DateTime.Now.AddMinutes(-13));
+            Message m4 = new Message("H2i", "Mary", DateTime.Now.AddMinutes(-13));
+            Message m5 = new Message("H3i", "Mary", DateTime.Now.AddMinutes(-13));
+            for (int i = 0; i < 100; i++)
+            {
+                m1.AddLike();
+            }
+            m2.ShowInfo();
+
+            m1.WhoIsPopularity(m2);
+            m2.WhoIsPopularity(m1);
+            m1.WhoIsPopularity(m1);
+            m2.WhoIsPopularity(m2);
+            List<Message> list = new List<Message>();
+            list.Add(m2);
+            list.Add(m3);
+            list.Add(m4);
+            list.Add(m5);
+            m1.WhoIsPopularityN(list);
+        }
+    }
+}
